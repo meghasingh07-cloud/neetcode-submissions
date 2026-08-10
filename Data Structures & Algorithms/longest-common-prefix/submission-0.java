aea9c5e4de2c0790708158d07833304b6769a0c2
meghasingh07-cloud <meghasingh5775@gmail.com>
2026-08-11 02:11:48 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String first= strs[0];
+        for(int i=0; i<first.length();i++){
+            char ch=first.charAt(i);
+        
+        for(int j=1; j<strs.length; j++){
+            String currentWord = strs[j];
+        
+        if(i>=currentWord.length() || ch!= currentWord.charAt(i)){
+            return first.substring(0,i);
+
+        }
+        }
+        }
+        return first;
+    }
+}
